eaa71060409fe8a7c4cf6a6fb51bfd6600ae1b22
Marie COLPART <120127767+Kasaaii@users.noreply.github.com>
2024-02-02 21:55:15 +0100

    Update script.js

--- COLPART_MARIE_GR5_Javascript/js/script.js
+++ COLPART_MARIE_GR5_Javascript/js/script.js
@@ -20,3 +20,39 @@ function nightMode() {
 }
 
 nm.addEventListener("click", nightMode);
+
+let img = document.querySelector('#img1');
+
+function carousel() {
+    let is=img.getAttribute('src');
+    console.log(is);
+    if(is=="img/hello.jpg"){
+        img.setAttribute('src',"img/mymelo.jpg");
+    }
+    if(is=="img/mymelo.jpg"){
+        img.setAttribute('src',"img/kuromi.jpg");
+    }
+    if(is=="img/kuromi.jpg"){
+        img.setAttribute('src',"img/cinna.jpg");
+    }
+    if(is=="img/cinna.jpg"){
+        img.setAttribute('src',"img/pompom.jpg");
+    }
+    if(is=="img/pompom.jpg"){
+        img.setAttribute('src',"img/pocha.jpg");
+    }
+    if(is=="img/pocha.jpg"){
+        img.setAttribute('src',"img/kerro.jpg");
+    }
+    if(is=="img/kerro.jpg"){
+        img.setAttribute('src',"img/sam.jpg");
+    }
+    if(is=="img/sam.jpg"){
+        img.setAttribute('src',"img/kila.jpg");
+    }
+    if(is=="img/kila.jpg"){
+        img.setAttribute('src',"img/hello.jpg");
+    }
+}
+
+img.addEventListener('click',carousel)
